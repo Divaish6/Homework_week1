bff5150dbe92dc93d49b9c14173034349daad07e
Divaish6 <Diivaish28@gmail.com>
2023-05-08 19:21:16 +0100

    homework submited

---
+++ homework_week1_divyesh/Program10.java
@@ -0,0 +1,11 @@
+package homework_week1_divyesh;
+
+public class Program10 {
+    public static void main (String[] args){
+        System.out.println("input first number");
+        int num1 = 25;
+        System.out.println("input second number");
+        int num2 = 5;
+        System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+    }
+}
